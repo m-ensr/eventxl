ab3025ac2eafc891d17f8f8c24540363648cf4fd
m-ensr <ensar.ozdemir@yahoo.com>
2023-12-11 01:04:06 +0300

    Update Header.jsx

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,13 +1,15 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import './Header.css';
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Header.css";
 
 function Header() {
   return (
     <div>
-      <Link to="/" className='header'> 🎭 Event-XL 🎬 </Link>
+      <Link to="/" className="header">
+        🎭 Event-XL 🎬
+      </Link>
     </div>
-  )
+  );
 }
 
 export default Header;
